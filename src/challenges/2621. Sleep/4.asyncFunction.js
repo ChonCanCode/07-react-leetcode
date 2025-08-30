52b0729af60ffb99a6d001a04085514d90791e01
ChonCanCode <163328241+ChonCanCode@users.noreply.github.com>
2025-08-30 07:57:36 +0100

    sleep 4.1

--- src/challenges/2621. Sleep/4.asyncFunction.js
+++ src/challenges/2621. Sleep/4.asyncFunction.js
@@ -8,46 +8,46 @@
 
 //    Test by logging the result.
 
-// 1. What should happen? (features / behaviors)
-// Async function return a value after delay it used to fetching dat from API.
+//my approach 01
+// async function delayValue(value, ms) {
+//   return await new Promise((resolve) => {
+//     setTimeout((value) => {
+//       resolve(value);
+//     }, ms);
+//   });
+// }
 
-// 2. What do I need to remember? (state / variables)
-// It needs to eb able to capture the value
+// console.log(delayValue("testing", 1000));
 
-// 3. How does it change over time? (loops, timers, events)
-// Doesn't need change simply return one value
+//My approach 2
+// function wait(ms) {
+//   return new Promise(resolve => setTimeout(resolve, ms))
+// }
 
-// 4. How do I organize it? (functions, objects, modules)
-// single async function > await
+// async function delayedValue(value){
+//   await return value + wait()
+// }
 
-async function delayValue(value, ms) {
-  return await setTimeout((value) => {
-    value;
-  }, ms);
-}
+//Laughg die me loging
+// function wait(ms) {
+//   return new Promise((resolve) => setTimeout(resolve, ms));
+// }
 
-delayValue("Result", 1000);
+// async function delayedValue(value, ms) {
+//   await wait(ms);
+//   return value;
+// }
 
-//first answer
-// helper wait function (from Q3)
+// console.log("Hello World", 1000);
+
+//Correct approch
 function wait(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-// main async function
 async function delayedValue(value, ms) {
-  await wait(ms); // wait for ms ms
-  return value; // then return the value
+  await wait(ms);
+  return console.log(value);
 }
 
-// test
-delayedValue("Result", 1000).then(console.log);
-
-//second answer
-async function delayedValue(value, ms) {
-  return new Promise((resolve) => {
-    setTimeout(() => resolve(value), ms);
-  });
-}
-
-delayedValue("Result", 1000).then(console.log);
+delayedValue("Hello World", 1000);
