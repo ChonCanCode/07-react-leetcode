605a0bfe921e4d461f77f2abdb501edc26a35ba4
ChonCanCode <163328241+ChonCanCode@users.noreply.github.com>
2025-09-07 11:19:01 +0100

    learning to write better code

--- src/challenges/2621. Sleep/6.implementSleep.js
+++ src/challenges/2621. Sleep/6.implementSleep.js
@@ -45,33 +45,55 @@
 // });
 
 // fuynct;
-let startTimer;
-let stopTimer;
-let elapsed;
+// let startTimer;
+// let stopTimer;
+// let elapsed;
 
-async function sleep(millis) {
-  let answer = "Hello World!";
-  startTimer = Date.now();
-  await new Promise((resolve) => setTimeout(() => resolve(answer), millis));
-  stopTimer = Date.now();
-  elapsed = stopTimer - startTimer;
-  let secondsElapsed = Math.trunc(elapsed / 1000);
-  console.log(answer, "Took:", secondsElapsed, "seconds");
-}
+// async function sleep(millis) {
+//   let answer = "Hello World!";
+//   startTimer = Date.now();
+//   await new Promise((resolve) => setTimeout(() => resolve(answer), millis));
+//   stopTimer = Date.now();
+//   elapsed = stopTimer - startTimer;
+//   let secondsElapsed = Math.trunc(elapsed / 1000);
+//   console.log(answer, "Took:", secondsElapsed, "seconds");
+// }
 
-sleep(5000);
+// sleep(5000);
 
 // Professional rewrite:
 
-async function sleepPro(millis) {
+// async function sleepPro(millis) {
+//   const start = Date.now();
+
+//   await new Promise((resolve) => setTimeout(resolve, millis));
+
+//   const elapsed = Date.now() - start;
+//   const seconds = Math.round(elapsed / 1000);
+
+//   console.log(`Pro:Hello World! Took: ${seconds} seconds`);
+// }
+
+// sleepPro(5000);
+
+const sleep = (ms) =>
+  new Promise((resolve) => {
+    const start = Date.now();
+    setTimeout(() => {
+      const elapsed = (Date.now() - start) / 1000;
+      resolve(`Sleep function took ${elapsed.toFixed(1)} seconds to complete`);
+    }, ms);
+  });
+
+sleep(500).then(console.log);
+
+const sleepProStyle = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+async function demo(ms) {
   const start = Date.now();
-
-  await new Promise((resolve) => setTimeout(resolve, millis));
-
-  const elapsed = Date.now() - start;
-  const seconds = Math.round(elapsed / 1000);
-
-  console.log(`Pro:Hello World! Took: ${seconds} seconds`);
+  await sleepProStyle(ms);
+  const elapsed = (Date.now() - start) / 1000;
+  return `Sleep function took ${elapsed.toFixed(1)} seconds to complete`;
 }
 
-sleepPro(5000);
+demo(500).then(console.log);
